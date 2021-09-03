0e096ae347857148857faa34d006efd763fa8a0c
oddtomas <oddthomas@csu.fullerton.edu>
2021-09-03 00:25:18 -0700

    changed header nav

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { Nav, NavItem } from "reactstrap";
 
 class Header extends Component {
   render() {
@@ -8,32 +9,45 @@ class Header extends Component {
       <header>
         <br></br>
         <br></br>
-        <NavLink to="/">Home</NavLink>
-
-        <NavLink to="/apartmentsindex" className="nav-link">
-          See All the Apartments
-        </NavLink>
-
-        {logged_in && (
-          <a href={sign_out_route} className="nav-link">
-            Sign Out
-          </a>
-        )}
-        {!logged_in && (
-          <a href={sign_in_route} className="nav-link">
-            Sign In
-          </a>
-        )}
-        {logged_in && (
-          <NavLink to="/myapartments" className="nav-link">
-            My Apartments
+        <Nav pills>
+          <NavItem></NavItem>
+          <NavLink to="/" className="nav-link">
+            Home
           </NavLink>
-        )}
-        {logged_in && (
-          <NavLink to="/apartmentnew" className="nav-link">
-            Add an Apartment
-          </NavLink>
-        )}
+          <NavItem>
+            <NavLink to="/apartmentsindex" className="nav-link">
+              See All the Apartments
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            {logged_in && (
+              <a href={sign_out_route} className="nav-link">
+                Sign Out
+              </a>
+            )}
+          </NavItem>
+          <NavItem>
+            {!logged_in && (
+              <a href={sign_in_route} className="nav-link">
+                Sign In
+              </a>
+            )}
+          </NavItem>
+          <NavItem>
+            {logged_in && (
+              <NavLink to="/myapartments" className="nav-link">
+                My Apartments
+              </NavLink>
+            )}
+          </NavItem>
+          <NavItem>
+            {logged_in && (
+              <NavLink to="/apartmentnew" className="nav-link">
+                Add an Apartment
+              </NavLink>
+            )}
+          </NavItem>
+        </Nav>
       </header>
     );
   }
